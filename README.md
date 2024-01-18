e68794e409138356c4042335ac3f0db014abcd88
SamantaDaugule <144602286+SamantaDaugule@users.noreply.github.com>
2024-01-18 13:27:02 +0000

    main

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # excel-sort
 ## Projekta uzdevums
+Projekta uzdevums ir sakārtot Excel faila kolonnu alfabētiski, skaitliski vai pēc datuma, augošā vai dilstošā secībā, atkarībā no lietotāja prasītā un saglabāt jaunā lapā. Projektam ir pievienots piemēra Excel fails.
+## Izmantotās bibliotēkas
+### Izmantotas sys un pandas bibliotēkas
+**sys** bibliotēka tika izmantota, lai izietu no programmas, ja lietotājs ievadījis nepareizus vai kļūdainus datus.
+**pandas** bibliotēka tika izmantota datu apstrādei un Excel faila lasīšanai un papildināšanai.
+## Programmatūras izmantošanas metode
+1. Izpilda skriptu un ievada Excel faila pilno nosaukumu, ievērojot simbolus, lielos un mazos burtus.
+2. Ievada faila lapas nosaukumu, ievērojot simbolus, lielos un mazos burtus.
+2. Ievada, pēc kādiem parametriem vēlas sakārtot kolonnu, izvēloties vienu no piedāvātajām opcijām, ievērojot galotni. Netiek ievēroti lielie un mazie burti, garumzīmes.
+3. Ievada, vai datus kārtot augošā vai dilstošā secībā, ievērojot galotni. Netiek ievēroti lielie un mazie burti, garumzīmes.
+4. Ievada tās kolonnas, kuru vēlas sakārtot, nosaukumu. Kolonnas nosaukumam jābūt uzrakstītam tā pat, kā Excel failā
+5. Ievada nosaukumu Excel lapai, kurā sakārtotie dati tiks saglabāti.
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import sys
 import pandas as pd
+nam=input("Ievadiet Excel faila nosaukumu: ")
+sh=input("Ievadiet faila lapas nosaukumu: ")
 try:
-    ex=pd.read_excel('example.xlsx')
+    ex=pd.read_excel(nam, sheet_name=sh)
+except FileNotFoundError:
+    print(f"Failu {nam} nevarēja atrast")
+    sys.exit()
 except Exception as e:
     print(f"Kļūda lasot failu: {e}")
     sys.exit()
 into=input("Kā vēlaties sakārtot kolonnu? (Alfabētiski, skaitliski, datums) ")
-if(into == 'alfabētiski' or  into == 'alfabetiski' ):
+if(into.lower() == 'alfabētiski' or  into.lower() == 'alfabetiski' ):
     sort=input("Augoši vai dilstoši? ")
-    if(sort == 'augoši' or sort == 'augosi' ):
+    if(sort.lower() == 'augoši' or sort.lower() == 'augosi' ):
         col=input("Kura kolonna? (Ierakstiet kolonnas nosaukumu) ")
         try:
-            ex=ex.sort_values(by=col, na_position='last')
+            if ex[col].dtype == 'object':
+                ex['temp'] = ex[col].astype(str)
+                ex['temp'] = ex['temp'].str.extract('(\D+)')
+                ex['temp'] = ex['temp'].fillna('')
+                ex = ex.sort_values(by='temp', na_position='last')
+                ex = ex.drop(columns=['temp'])
         except KeyError:
             print(f"Kolonna {col} nav atrasta")
             sys.exit()
@@ -19,12 +29,17 @@ if(into == 'alfabētiski' or  into == 'alfabetiski' ):
             print(f"Kļūda: {e}")
             sys.exit()
         name=input("Ierakstiet nosaukumu lapai, kurā saglabāt sakārtoto: ")
-        with pd.ExcelWriter('example.xlsx',mode='a') as writer:  
+        with pd.ExcelWriter(nam,mode='a') as writer:  
            ex.to_excel(writer, sheet_name=name, index=False)
-    elif(sort == 'dilstoši' or sort == 'dilstosi' ):
+    elif(sort.lower() == 'dilstoši' or sort.lower() == 'dilstosi' ):
         col=input("Kura kolonna? (Ierakstiet kolonnas nosaukumu) ")
         try:
-            ex=ex.sort_values(by=col,ascending=False, na_position='last')
+            if ex[col].dtype == 'object':
+                ex['temp'] = ex[col].astype(str)
+                ex['temp'] = ex['temp'].str.extract('(\D+)')
+                ex['temp'] = ex['temp'].fillna('')
+                ex = ex.sort_values(by='temp',ascending=False, na_position='last')
+                ex = ex.drop(columns=['temp'])
         except KeyError:
             print(f"Kolonna {col} nav atrasta")
             sys.exit()
@@ -32,17 +47,22 @@ if(into == 'alfabētiski' or  into == 'alfabetiski' ):
             print(f"Kļūda: {e}")
             sys.exit()
         name=input("Ierakstiet nosaukumu lapai, kurā saglabāt sakārtoto: ")
-        with pd.ExcelWriter('example.xlsx',mode='a') as writer:  
+        with pd.ExcelWriter(nam,mode='a') as writer:  
            ex.to_excel(writer, sheet_name=name, index=False)
     else:
         print("Lūdzu ierakstiet vienu no piedāvātajām opcijām")
         sys.exit()
 elif(into == 'skaitliski' ):
     sort=input("Augoši vai dilstoši? ")
-    if(sort == 'augoši' or sort == 'augosi' ):
+    if(sort.lower() == 'augoši' or sort.lower() == 'augosi' ):
         col=input("Kura kolonna? (Ierakstiet kolonnas nosaukumu) ")
         try:
-            ex=ex.sort_values(by=col, na_position='last')
+            if ex[col].dtype in ['int64', 'float64']:
+                ex['temp'] = ex[col].astype(str)
+                ex['temp'] = ex['temp'].str.replace('[^\d.]', '', regex=True)
+                ex['temp'] = ex['temp'].replace('', 'NaN').astype(float)
+                ex = ex.sort_values(by='temp', na_position='last')
+                ex = ex.drop(columns=['temp'])
         except KeyError:
             print(f"Kolonna {col} nav atrasta")
             sys.exit()
@@ -50,12 +70,17 @@ elif(into == 'skaitliski' ):
             print(f"Kļūda: {e}")
             sys.exit()
         name=input("Ierakstiet nosaukumu lapai, kurā saglabāt sakārtoto: ")
-        with pd.ExcelWriter('example.xlsx',mode='a') as writer:  
+        with pd.ExcelWriter(nam,mode='a') as writer:  
             ex.to_excel(writer, sheet_name=name, index=False)
-    elif(sort == 'dilstoši' or sort == 'dilstosi' ):
+    elif(sort.lower() == 'dilstoši' or sort.lower() == 'dilstosi' ):
         col=input("Kura kolonna? (Ierakstiet kolonnas nosaukumu) ")
         try:
-            ex=ex.sort_values(by=col,ascending=False, na_position='last')
+            if ex[col].dtype in ['int64', 'float64']:
+                ex['temp'] = ex[col].astype(str)
+                ex['temp'] = ex['temp'].str.replace('[^\d.]', '', regex=True)
+                ex['temp'] = ex['temp'].replace('', 'NaN').astype(float)
+                ex = ex.sort_values(by='temp',ascending=False, na_position='last')
+                ex = ex.drop(columns=['temp'])
         except KeyError:
             print(f"Kolonna {col} nav atrasta")
             sys.exit()
@@ -63,17 +88,26 @@ elif(into == 'skaitliski' ):
             print(f"Kļūda: {e}")
             sys.exit()
         name=input("Ierakstiet nosaukumu lapai, kurā saglabāt sakārtoto: ")
-        with pd.ExcelWriter('example.xlsx',mode='a') as writer:  
+        with pd.ExcelWriter(nam,mode='a') as writer:  
             ex.to_excel(writer, sheet_name=name, index=False)
     else:
         print("Lūdzu ierakstiet vienu no piedāvātajām opcijām")
         sys.exit()
 elif(into == 'datums' ):
     sort=input("Augoši vai dilstoši? ")
-    if(sort == 'augoši' or sort == 'augosi' ):
+    if(sort.lower() == 'augoši' or sort.lower() == 'augosi' ):
         col=input("Kura kolonna? (Ierakstiet kolonnas nosaukumu) ")
         try:
-            ex=ex.sort_values(by=col, na_position='last')
+            if ex[col].dtype == 'datetime64[ns]':
+                ex = ex.sort_values(by=col, ascending=True, na_position='last')
+            else:
+                try:
+                    ex['temp'] = pd.to_datetime(ex[col], errors='coerce')
+                    ex = ex.sort_values(by='temp', ascending=True, na_position='last')
+                    ex = ex.drop(columns=['temp'])
+                except ValueError:
+                    print(f"Kolonnas {col} vērtības nevar pārvērst par datumu")
+                    sys.exit()
         except KeyError:
             print(f"Kolonna {col} nav atrasta")
             sys.exit()
@@ -81,12 +115,21 @@ elif(into == 'datums' ):
             print(f"Kļūda: {e}")
             sys.exit()
         name=input("Ierakstiet nosaukumu lapai, kurā saglabāt sakārtoto: ")
-        with pd.ExcelWriter('example.xlsx',mode='a') as writer:  
+        with pd.ExcelWriter(nam,mode='a') as writer:  
            ex.to_excel(writer, sheet_name=name, index=False)
-    elif(sort == 'dilstoši' or sort == 'dilstosi' ):
+    elif(sort.lower() == 'dilstoši' or sort.lower() == 'dilstosi' ):
         col=input("Kura kolonna? (Ierakstiet kolonnas nosaukumu) ")
         try:
-            ex=ex.sort_values(by=col,ascending=False, na_position='last')
+            if ex[col].dtype == 'datetime64[ns]':
+                ex = ex.sort_values(by=col, ascending=False, na_position='last')
+            else:
+                try:
+                    ex['temp'] = pd.to_datetime(ex[col], errors='coerce')
+                    ex = ex.sort_values(by='temp', ascending=False, na_position='last')
+                    ex = ex.drop(columns=['temp'])
+                except ValueError:
+                    print(f"Kolonnas {col} vērtības nevar pārvērst par datumu")
+                    sys.exit()
         except KeyError:
             print(f"Kolonna {col} nav atrasta")
             sys.exit()
@@ -94,7 +137,7 @@ elif(into == 'datums' ):
             print(f"Kļūda: {e}")
             sys.exit()
         name=input("Ierakstiet nosaukumu lapai, kurā saglabāt sakārtoto: ")
-        with pd.ExcelWriter('example.xlsx',mode='a') as writer:  
+        with pd.ExcelWriter(nam,mode='a') as writer:  
            ex.to_excel(writer, sheet_name=name, index=False)
     else:
         print("Lūdzu ierakstiet vienu no piedāvātajām opcijām")
